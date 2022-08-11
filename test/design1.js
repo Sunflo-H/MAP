a00d404708550bf91accabf5f1f8c61e07be12bf
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2022-08-11 21:16:39 +0900

    panTo 적용

--- test/design1.js
+++ test/design1.js
@@ -368,7 +368,11 @@ function 마커클릭후정보를띄우기() {
 
 }
 
-
+function panTo(lat, lng) {
+    // 지도 중심을 부드럽게 이동시킵니다
+    // 만약 이동할 거리가 지도 화면보다 크면 부드러운 효과 없이 이동합니다
+    map.panTo(new Tmapv2.LatLng(lat, lng));
+}
 
 function getUserLocation() {
     return new Promise((resolve, reject) => {
