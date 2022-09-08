21767d031c215295bf6cb8bbbabcb68aa99f9e6c
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2022-09-08 16:58:47 +0900

    검색 기능 모듈화: kakaoSearchModule.js 이름의 모듈 파일을 만들고 클래스로 검색기능을 정의함

--- assets/js/kakaoSearchModule.js
+++ assets/js/kakaoSearchModule.js
@@ -1,114 +1,121 @@
-/**
- * 필요한 변수
- * map, SEARCH_DATA_LENGTH
- */
+export default class kakaoSearch {
+    constructor() {
+        this.SEARCH_DATA_LENGTH = 15;
+        this.RADIUS = {
+            LV1: 5000,
+            LV2: 10000,
+            LV3: 15000,
+            LV4: 20000
+        }
+    }
+    getA(){
+        return 'A';
+    }
+    /**
+     * 주소로검색, 키워드로검색 두 함수를 한번에 사용하여 검색한다.
+     * @param {*} keyword 입력한 단어
+     * @returns 두 검색방법으로 찾은 데이터
+     */
+    search(keyword, lat, lng) {
+        let data = Promise.all([this.searchByAddr(keyword), this.searchByKeyword(keyword, lat, lng)])
+            .then(data => data);
+        console.log(data);
+        return data;
+    }
 
-/**
- * 주소로검색, 키워드로검색 두 함수를 한번에 사용하여 검색한다.
- * @param {*} keyword 입력한 단어
- * @returns 두 검색방법으로 찾은 데이터
- */
- function search(keyword, map) {
-    let data = Promise.all([searchByAddr(keyword), searchByKeyword(keyword, map)])
-                .then(data => data);
-    console.log(data);
-    return data;
-}
+    /**
+     * 주소로 검색하여 장소(주소)에 대한 정보를 얻는다.
+     * @param {*} addr 검색할 주소 ex)구의동
+     * @returns promise [주소데이터1, 주소데이터2 ...]
+     */
+    searchByAddr(addr) {
+        let placeList = new Promise(resolve => {
 
-/**
- * 주소로 검색하여 장소(주소)에 대한 정보를 얻는다.
- * @param {*} addr 검색할 주소 ex)구의동
- * @returns promise [주소데이터1, 주소데이터2 ...]
- */
- function searchByAddr(addr) {
-    let placeList = new Promise(resolve => {
+            // 주소-좌표 변환 객체를 생성합니다
+            let geocoder = new kakao.maps.services.Geocoder();
 
-        // 주소-좌표 변환 객체를 생성합니다
-        let geocoder = new kakao.maps.services.Geocoder();
+            const callback = (result, status) => {
+                if (status === kakao.maps.services.Status.OK) {
+                    console.log(result);
+                }
+                resolve(result);
+            };
 
-        const callback = (result, status) => {
-            if (status === kakao.maps.services.Status.OK) {
-                console.log(result);
+            geocoder.addressSearch(addr, callback, { size: this.SEARCH_DATA_LENGTH });
+        });
+        return placeList;
+    }
+
+    /**
+     * 키워드로 검색하여 장소(주소)에 대한 정보를 얻는다.
+     * @param {*} keyword 검색할 키워드 ex)롯데리아
+     * @returns promise [장소데이터1, 장소데이터2, ...]
+     */
+    searchByKeyword(keyword, lat, lng) {
+        // const lat = map.getCenter()._lat;
+        // const lng = map.getCenter()._lng;
+
+        let placeList = new Promise((resolve, reject) => {
+            let places = new kakao.maps.services.Places();
+
+            const getResult = (result, status) => {
+                if (status === kakao.maps.services.Status.OK) {
+                    resolve(result);
+                }
             }
-            resolve(result);
-        };
 
-        geocoder.addressSearch(addr, callback, {size: SEARCH_DATA_LENGTH});
-    });
-    return placeList;
+            let option = {
+                x: lng,
+                y: lat,
+                radius: this.RADIUS.LV4,
+                size: this.SEARCH_DATA_LENGTH
+            }
+            places.keywordSearch(keyword, getResult, option);
+        });
+        return placeList;
+    }
+
+    /**
+     * 좌표로 행정동 주소 정보를 요청합니다. 동까지의 주소 정보를 얻는다.
+     * @param {*} lat 위도 y
+     * @param {*} lng 경도 x
+     * @returns promise [{기본주소(서울 광진구 구의동)}, {상세주소(서울 광진구 구의2동)}]
+     */
+    searchAddrFromCoords(lat, lng) {
+        let addr = new Promise((resolve, reject) => {
+            var geocoder = new kakao.maps.services.Geocoder();
+
+            const getResult = (result, status) => {
+                if (status === kakao.maps.services.Status.OK) {
+                    resolve(result);
+                }
+            };
+
+            geocoder.coord2RegionCode(lng, lat, getResult);
+        });
+
+        return addr;
+    }
+
+    /**
+     * 좌표로 법정동 상세 주소 정보를 요청합니다
+     * @param {*} lat 위도 y
+     * @param {*} lng 경도 x
+     * @returns promise [{지번주소, 도로명주소}]
+     */
+    searchDetailAddrFromCoords(lat, lng) {
+        let detailAddr = new Promise((resolve, reject) => {
+            var geocoder = new kakao.maps.services.Geocoder();
+
+            const getResult = (result, status) => {
+                if (status === kakao.maps.services.Status.OK) {
+                    resolve(result);
+                }
+            };
+
+            geocoder.coord2Address(lng, lat, getResult);
+        });
+
+        return detailAddr;
+    }
 }
-
-/**
- * 키워드로 검색하여 장소(주소)에 대한 정보를 얻는다.
- * @param {*} keyword 검색할 키워드 ex)롯데리아
- * @returns promise [장소데이터1, 장소데이터2, ...]
- */
- function searchByKeyword(keyword) {
-    const lat = map.getCenter()._lat;
-    const lng = map.getCenter()._lng;
-
-    let placeList = new Promise((resolve, reject) => {
-        let places = new kakao.maps.services.Places();
-
-        const getResult = (result, status) => {
-            if (status === kakao.maps.services.Status.OK) {
-                resolve(result);
-            }  
-        }
-
-        let option = {
-            x: lng,
-            y: lat,
-            radius: RADIUS.LV4,
-            size: SEARCH_DATA_LENGTH
-        }
-        places.keywordSearch(keyword, getResult, option);
-    });
-    return placeList;
-}
-
-/**
- * 좌표로 행정동 주소 정보를 요청합니다. 동까지의 주소 정보를 얻는다.
- * @param {*} lat 위도 y
- * @param {*} lng 경도 x
- * @returns promise [{기본주소(서울 광진구 구의동)}, {상세주소(서울 광진구 구의2동)}]
- */
- function searchAddrFromCoords(lat, lng) {
-    let addr = new Promise((resolve, reject) => {
-        var geocoder = new kakao.maps.services.Geocoder();
-
-        const getResult = (result, status) => {
-            if (status === kakao.maps.services.Status.OK) {
-                resolve(result);
-            }  
-        };
-    
-        geocoder.coord2RegionCode(lng, lat, getResult);         
-    });
-
-    return addr;
-}
-
-/**
- * 좌표로 법정동 상세 주소 정보를 요청합니다
- * @param {*} lat 위도 y
- * @param {*} lng 경도 x
- * @returns promise [{지번주소, 도로명주소}]
- */
- function searchDetailAddrFromCoords(lat, lng) {
-    let detailAddr = new Promise((resolve, reject) => {
-        var geocoder = new kakao.maps.services.Geocoder();
-
-        const getResult = (result, status) => {
-            if (status === kakao.maps.services.Status.OK) {
-                resolve(result);
-            }  
-        };
-
-        geocoder.coord2Address(lng, lat, getResult);        
-    });
-
-    return detailAddr;
-}
-
-export {search, searchByAddr, searchByKeyword, searchAddrFromCoords, searchDetailAddrFromCoords } ;
--- index.js
+++ index.js
@@ -9,14 +9,10 @@
 
 // 다 만든다음에 크롤링이니, php, mysql이니 생각을 해보자고
 
-import * as kakaoSearch from './assets/js/kakaoSearchModule.js';
+import kakaoSearchModule from './assets/js/kakaoSearchModule.js';
 
-const RADIUS = {
-    LV1: 5000,
-    LV2: 10000,
-    LV3: 15000,
-    LV4: 20000
-}
+const kakaoSearch = new kakaoSearchModule();
+
 const SEARCH_DATA_LENGTH = 15; // 15가 MAX
 
 
@@ -536,7 +532,7 @@ function categorySearch(event) {
     // 공공기관 코드 검색, 찾은 placeList는 callback으로 전달한다.
     places.categorySearch(categoryCode, callback, {
         location: location,
-        size: SEARCH_DATA_LENGTH
+        size: kakaoSearch.SEARCH_DATA_LENGTH
     });
 }
 
@@ -742,114 +738,6 @@ function getJsonData(keyword) {
     return result;
 }
 
-/**
- * 주소로검색, 키워드로검색 두 함수를 한번에 사용하여 검색한다.
- * @param {*} keyword 입력한 단어
- * @returns 두 검색방법으로 찾은 데이터
- */
-// function search(keyword) {
-//     let data = Promise.all([searchByAddr(keyword), searchByKeyword(keyword)]).then(data => data);
-//     console.log(data);
-//     return data;
-// }
-
-/**
- * 주소로 검색하여 장소(주소)에 대한 정보를 얻는다.
- * @param {*} addr 검색할 주소 ex)구의동
- * @returns promise [주소데이터1, 주소데이터2 ...]
- */
-// function searchByAddr(addr) {
-//     let placeList = new Promise((resolve, reject) => {
-
-//         // 주소-좌표 변환 객체를 생성합니다
-//         let geocoder = new kakao.maps.services.Geocoder();
-
-//         const callback = (result, status) => {
-//             if (status === kakao.maps.services.Status.OK) {
-//                 console.log(result);
-//             }
-//             resolve(result);
-//         };
-
-//         geocoder.addressSearch(addr, callback, {size: SEARCH_DATA_LENGTH});
-//     });
-//     return placeList;
-// }
-
-/**
- * 키워드로 검색하여 장소(주소)에 대한 정보를 얻는다.
- * @param {*} keyword 검색할 키워드 ex)롯데리아
- * @returns promise [장소데이터1, 장소데이터2, ...]
- */
-// function searchByKeyword(keyword) {
-//     const lat = map.getCenter()._lat;
-//     const lng = map.getCenter()._lng;
-
-//     let placeList = new Promise((resolve, reject) => {
-//         let places = new kakao.maps.services.Places();
-
-//         const getResult = (result, status) => {
-//             if (status === kakao.maps.services.Status.OK) {
-//                 resolve(result);
-//             }  
-//         }
-
-//         let option = {
-//             x: lng,
-//             y: lat,
-//             radius: RADIUS.LV4,
-//             size: SEARCH_DATA_LENGTH
-//         }
-//         places.keywordSearch(keyword, getResult, option);
-//     });
-//     return placeList;
-// }
-
-
-/**
- * 좌표로 행정동 주소 정보를 요청합니다. 동까지의 주소 정보를 얻는다.
- * @param {*} lat 위도 y
- * @param {*} lng 경도 x
- * @returns promise [{기본주소(서울 광진구 구의동)}, {상세주소(서울 광진구 구의2동)}]
- */
-// function searchAddrFromCoords(lat, lng) {
-//     let addr = new Promise((resolve, reject) => {
-//         var geocoder = new kakao.maps.services.Geocoder();
-
-//         const getResult = (result, status) => {
-//             if (status === kakao.maps.services.Status.OK) {
-//                 resolve(result);
-//             }  
-//         };
-    
-//         geocoder.coord2RegionCode(lng, lat, getResult);         
-//     });
-
-//     return addr;
-// }
-
-/**
- * 좌표로 법정동 상세 주소 정보를 요청합니다
- * @param {*} lat 위도 y
- * @param {*} lng 경도 x
- * @returns promise [{지번주소, 도로명주소}]
- */
-// function searchDetailAddrFromCoords(lat, lng) {
-//     let detailAddr = new Promise((resolve, reject) => {
-//         var geocoder = new kakao.maps.services.Geocoder();
-
-//         const getResult = (result, status) => {
-//             if (status === kakao.maps.services.Status.OK) {
-//                 resolve(result);
-//             }  
-//         };
-
-//         geocoder.coord2Address(lng, lat, getResult);        
-//     });
-
-//     return detailAddr;
-// }
-
 /** isTrue의 값에 따라 검색리스트를 보여준다. */
 function displaySearchList(isTrue) {
     const searchList = document.querySelector('.interaction-container .searchList');
@@ -942,7 +830,10 @@ function setAutoComplete(data, searchContainerType) {
  * search를 실행하여 검색 결과로 마커, 검색컨텐츠를 세팅한다.
  */
 function enterKey() {
-    kakaoSearch.search(searchInMap.value, map)
+    const lat = map.getCenter()._lat;
+    const lng = map.getCenter()._lng;
+
+    kakaoSearch.search(searchInMap.value, lat, lng)
     .then(data => {
         const addressSearchData = data[0];
         const keywordSearchData = data[1];
